993ac27dd50f8d639ed2be0d2d96a8e8869e3909
CrNono <cristianzapatamanriquez@gmail.com>
2020-11-12 19:30:43 -0300

    first commit

---
+++ src/main/java/com/nonocode/SpringAngularPostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.nonocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularPostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularPostgreSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nonocode/SpringAngularPostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonocode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularPostgreSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
